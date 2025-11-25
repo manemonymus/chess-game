535d1d251e5061dce495d0cda10db4180203a5d1
Anuj Nair <nair259@purdue.edu>
2025-11-24 22:15:31 -0500

    first moves

--- main.py
+++ main.py
@@ -43,3 +43,54 @@ for i in range(rows):
         board[-1][i]='wr'
     
 printBoard(board)
+alphaMap={
+    'a':0,
+    'b':1,
+    'c':2,
+    'd':3,
+    'e':4,
+    'f':5,
+    'g':6,
+    'h':7
+    
+
+}
+numMap={
+    '8':0,
+    '7':1,
+    '6':2,
+    '5':3,
+    '4':4,
+    '3':5,
+    '2':6,
+    '1':7
+}
+# Game loop
+while True:
+    move = input("Enter your move (e.g., 'e2 e4') or 'resign': ")
+    
+    if move == 'resign':
+        break
+    
+    print(f"You entered: {move}")
+    if " " in move:
+        moves = move.lower().split()
+        initialMove = moves[0]  
+        finalMove = moves[1]    
+        
+        from_row = numMap[initialMove[1]]  
+        from_col = alphaMap[initialMove[0]]  
+        
+        to_row = numMap[finalMove[1]]
+        to_col = alphaMap[finalMove[0]]
+        
+   
+        piece = board[from_row][from_col]
+        board[to_row][to_col] = piece
+        board[from_row][from_col] = '__'  
+        
+        
+    printBoard(board)
+
+
+
